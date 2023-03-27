f6ad99fcff13ee15f0365a1ca0d3e537e1b74dc4
Marina <marinaporfirio11@gmail.com>
2023-03-27 10:47:32 -0300

    Correção da Index.cshtml

--- MyLittlePony/Controllers/HomeController.cs
+++ MyLittlePony/Controllers/HomeController.cs
@@ -3,34 +3,42 @@ using Microsoft.AspNetCore.Mvc;
 using MyLittlePony.Models;
 using MyLittlePony.Services;
 namespace MyLittlePony.Controllers;
+
 public class HomeController : Controller
 {
-private readonly ILogger<HomeController> _logger;
-private readonly IPonyService _pokeService;
-public HomeController(ILogger<HomeController> logger, IPonyService ponyService)
-{
-_logger = logger;
-_ponyService = ponyService;
-}
-public IActionResult Index(string tipo)
-{
-var ponys = _ponyService.GetPonyDto();
-ViewData["filter"] = string.IsNullOrEmpty(tipo) ? "all" : tipo;
-return View(ponys);
-}
-public IActionResult Details(int Numero)
-{
-var pony = _ponyService.GetDetailedPony(Numero);
-return View(pony);
-}
-public IActionResult Privacy()
-{
-return View();
-}
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-public IActionResult Error()
-{
-return View(new ErrorViewModel { RequestId = Activity.Current?.Id
-?? HttpContext.TraceIdentifier });
-}
+    private readonly ILogger<HomeController> _logger;
+    private readonly IPonyService _ponyService;
+    public HomeController(ILogger<HomeController> logger, IPonyService ponyService)
+    {
+        _logger = logger;
+        _ponyService = ponyService;
+    }
+
+    public IActionResult Index(string tipo)
+    {
+        var ponys = _ponyService.GetPonyDto();
+        ViewData["filter"] = string.IsNullOrEmpty(tipo) ? "all" : tipo;
+        return View(ponys);
+    }
+
+    public IActionResult Details(int Numero)
+    {
+        var pony = _ponyService.GetDetailedPony(Numero);
+        return View(pony);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel
+        {
+            RequestId = Activity.Current?.Id
+        ?? HttpContext.TraceIdentifier
+        });
+    }
 }
--- MyLittlePony/Program.cs
+++ MyLittlePony/Program.cs
@@ -10,19 +10,26 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddSingleton<IPonyService, PonyService>();
 
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-app.UseExceptionHandler("/Home/Error");
-// The default HSTS value is 30 days. You may want to change this for production .....
-app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production .....
+    app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.UseAuthorization();
+
 app.UseSession();
+
 app.MapControllerRoute(
-name: "default",
-pattern: "{controller=Home}/{action=Index}/{id?}");
-app.Run()
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
